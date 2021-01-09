cdaa619ea51e3df0ba3920bacd61cc6e836d751a
Zekhniddin <zexjumayev@gmail.com>
2021-01-09 14:49:43 +0500

    Initial Commit

---
+++ UI-Task4/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UI-Task4
+//
+//  Created by Зехниддин on 09/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
